2ec428c45232d10ab38cd203cdd66cad2dbe60a2
Stepanus Rio Defa Ardiantoro <stepanusriodefaardian@gmail.com>
2023-10-20 12:23:01 +0700

    CREATE: New Layout Login and Connect Activity Main to Register

--- app/src/main/java/com/example/fullapp/MainActivity.java
+++ app/src/main/java/com/example/fullapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.fullapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainRegister.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
