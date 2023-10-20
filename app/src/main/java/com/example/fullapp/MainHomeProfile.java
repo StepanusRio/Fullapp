aeb9c41a91d077d193e6273683f7a83ee9bf77e0
Stepanus Rio Defa Ardiantoro <stepanusriodefaardian@gmail.com>
2023-10-20 13:06:46 +0700

    Add: Home Activity

---
+++ app/src/main/java/com/example/fullapp/MainHomeProfile.java
@@ -0,0 +1,14 @@
+package com.example.fullapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainHomeProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home_profile);
+    }
+}
